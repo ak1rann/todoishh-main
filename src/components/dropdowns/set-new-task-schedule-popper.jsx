a7cae4cb05df665728e4d3d2c72e2ddb76d87df6
evgeniybryzh <evgeniy@merge.rocks>
2024-01-06 14:56:28 +0200

    added calendar

--- src/components/dropdowns/set-new-task-schedule-popper.jsx
+++ src/components/dropdowns/set-new-task-schedule-popper.jsx
@@ -1,20 +1,24 @@
 import { ReactComponent as NextWeekIcon } from "assets/svg/next-week.svg";
 import { ReactComponent as NoDateIcon } from "assets/svg/none.svg";
 import { ReactComponent as SetScheduleIcon } from "assets/svg/set-schedule.svg";
-import { ReactComponent as WeekendIcon } from "assets/svg/weekend.svg";
+import { ReactComponent as WeekendIcon } from "assets/svg/weekend.svg"
 import { TodayIcon } from "components/today-icon";
 import moment from "moment";
 import "./light.scss";
 import "./main.scss";
+import { useState } from "react";
+import ReactCalendar from "rc-calendar";
 
 
 export const SetNewTaskSchedulePopper = ({ isQuickAdd, setShowPopup, setSchedule, closeOverlay, xPosition, yPosition, parentPosition }) => {
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [selectedDateVal, setSelectedDateValue] = useState();
   const setNext7days = () => {
-    setSchedule({ day: "Наступного тижня", date: moment().add(7, "днів").format("DD-MM-YYYY") });
+    setSchedule({ day: "Наступного тижня", date: moment().startOf('isoWeek').add(1, 'week').format("DD-MM-YYYY") });
     isQuickAdd ? setShowPopup(false) : closeOverlay();
   };
   const setTomorrow = () => {
-    setSchedule({ day: "завтра", date: moment().add(1, "днів").format("DD-MM-YYYY") });
+    setSchedule({ day: "завтра", date: moment().add(1, 'days').format("DD-MM-YYYY") });
     isQuickAdd ? setShowPopup(false) : closeOverlay();
   };
   const setToday = () => {
@@ -22,13 +26,24 @@ export const SetNewTaskSchedulePopper = ({ isQuickAdd, setShowPopup, setSchedule
     isQuickAdd ? setShowPopup(false) : closeOverlay();
   };
   const setWeekend = () => {
-    setSchedule({ day: "Вихідні", date: moment().day("Субота").format("DD-MM-YYYY") });
+    const getNextSunday = moment().day(6).format("DD-MM-YYYY") === moment().format("DD-MM-YYYY") ? moment().add(1, 'weeks').day(6).format("DD-MM-YYYY") : moment().day(6).format("DD-MM-YYYY")
+    setSchedule({ day: "Вихідні", date:  getNextSunday });
     isQuickAdd ? setShowPopup(false) : closeOverlay();
   };
   const setNoDate = () => {
     setSchedule({ day: "", date: "" });
     isQuickAdd ? setShowPopup(false) : closeOverlay();
   };
+
+  const setOpenCalendarPopuopHandler = () => {
+    setIsCalendarOpen(true)
+  }
+
+  const onOkHandler = (date) => {
+    const selectedDate = date.format('DD-MM-YYYY')
+    setSchedule({ day: selectedDate, date: selectedDate });
+    isQuickAdd ? setShowPopup(false) : closeOverlay();
+  }
 
   const targetedposition = parentPosition ? parentPosition : { x: xPosition, y: yPosition };
 
@@ -40,22 +55,29 @@ export const SetNewTaskSchedulePopper = ({ isQuickAdd, setShowPopup, setSchedule
         isQuickAdd ? setShowPopup(false) : closeOverlay(event);
       }}
     >
+
       <div
         className="set-schedule__popper"
         onClick={(event) => event.stopPropagation()}
         style={{ top: `${targetedposition.y + 40}px`, left: `${targetedposition.x}px` }}
       >
-        <ul>
+        {isCalendarOpen && (
+          <div className="set-schedule__overlay">
+            <h2> <span>{selectedDateVal?.format('DD-MM-YYYY')}</span></h2>
+            <ReactCalendar  showOk onOk={onOkHandler} showDateInput={false} onChange={(date)=>setSelectedDateValue(date)}/>
+          </div>
+        )}
+        {!isCalendarOpen && <ul>
           {/* // todo: change this class name to "scheduler-suggestions-item" */}
           <li className="set-schedule__popper--option" onClick={() => setToday()}>
-            <div className="">
+            <div className="set-schedule__icon">
               <TodayIcon color={"#25b84c"} />
             </div>
             <p className="set-new-task__schedule--name">Сьогодні</p>
             <p className="set-new-task__schedule--weekday">{moment().format("ddd")}</p>
           </li>
           <li className="set-schedule__popper--option" onClick={() => setTomorrow()}>
-            <div className="">
+            <div className="set-schedule__icon">
               <SetScheduleIcon fill={"#ff9a14"} />
             </div>
 
@@ -63,7 +85,7 @@ export const SetNewTaskSchedulePopper = ({ isQuickAdd, setShowPopup, setSchedule
             <p className="set-new-task__schedule--weekday">{moment().add(1, "day").format("ddd")}</p>
           </li>
           <li className="set-schedule__popper--option" onClick={() => setWeekend()}>
-            <div className="">
+            <div className="set-schedule__icon">
               <WeekendIcon fill={"#5297ff"} />
             </div>
 
@@ -71,25 +93,34 @@ export const SetNewTaskSchedulePopper = ({ isQuickAdd, setShowPopup, setSchedule
             <p className="set-new-task__schedule--weekday">сб</p>
           </li>
           <li className="set-schedule__popper--option" onClick={() => setNext7days()}>
-            <div className="">
+            <div className="set-schedule__icon">
               <NextWeekIcon fill={"#a970ff"} />
             </div>
 
             <p className="set-new-task__schedule--name">Наступного тижня</p>
-            <p className="set-new-task__schedule--weekday">{moment().add(7,"day").format("ddd MMM D ")}</p>
+            <p className="set-new-task__schedule--weekday">{moment().add(7, "day").format("ddd MMM D ")}</p>
 
           </li>
           <li className="set-schedule__popper--option" onClick={() => setNoDate()}>
-            <div className="">
+            <div className="set-schedule__icon">
               <NoDateIcon color={"grey"} />
             </div>
 
             <p className="set-new-task__schedule--name">Без дати</p>
           </li>
-        </ul>
+
+          <li className="set-schedule__popper--option" onClick={setOpenCalendarPopuopHandler}>
+            <div className="set-schedule__icon">
+              <TodayIcon color={"red"} />
+            </div>
+            <p className="set-new-task__schedule--name">Обрати дату</p>
+          </li>
+
+        </ul>}
+
       </div>
-      
+
     </div>
-    
+
   );
 };
